6a5915a8a6d95d6d4d3f3794dc0778ce65ebca5e
Flavian Gontier <flavian.gontier@gmail.com>
2020-02-25 18:10:49 +0100

    Adding uploads folder to AI API

--- bernerslee/appControllers.js
+++ bernerslee/appControllers.js
@@ -17,14 +17,9 @@ async function addExamination(req, res) {
     const doctor = req.body.doctor;
     const attachment = req.file.filename;
     const details = "";
-    console.log('examination start');
     const diagnosis = await services.doExamination(req.file.path, name);
-    console.log('diagnosis:');
-    console.log(diagnosis);
 
     const examination = new entities.Examination(name, doctor, date, attachment, diagnosis, details);
-    console.log('examination:');
-    console.log(examination);
 
     res.locals.patient.examinations.push(examination);
     await database.engine.collection('patients').updateOne({ _id: res.locals.patient._id }, {
@@ -35,8 +30,6 @@ async function addExamination(req, res) {
     });
 
     const entity = await database.engine.collection('patients').findOne({ _id: res.locals.patient._id });
-    console.log('entity:');
-    console.log(entity);
     return res.send(entity);
 }
 
